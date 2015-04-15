a3c6fc47249d3ad8959f1d23cc9ab676771f2169
Michael <Michael@MichaelMedion.fritz.box>
2015-04-15 12:53:18 +0200

    * patchnotes + version

--- src.executables/executables/clients/ClientMain.java
+++ src.executables/executables/clients/ClientMain.java
@@ -12,7 +12,7 @@ import model.database.Database;
 import network.tcp.messages.MessageRegister;
 
 public class ClientMain {
-	public static final String VERSION = "1.03";
+	public static final String VERSION = "2.00";
 
 	public static void main(String[] args) {
 		Database.init();
