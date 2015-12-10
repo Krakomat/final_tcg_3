dfe26c72bea1babd9bdfe70d75964de1ad284aa4
Michael <mfeldma2@mail.upb.de>
2015-12-10 20:29:02 +0100

    + LtSurge Scripts

---
+++ src.model/model/scripting/ltSurge/Script_00340_LtSurgesTreaty.java
@@ -0,0 +1,24 @@
+package model.scripting.ltSurge;
+
+import model.database.TrainerCard;
+import model.enums.PlayerAction;
+import model.interfaces.PokemonGame;
+import model.scripting.abstracts.TrainerCardScript;
+
+public class Script_00340_LtSurgesTreaty extends TrainerCardScript {
+
+	public Script_00340_LtSurgesTreaty(TrainerCard card, PokemonGame gameModel) {
+		super(card, gameModel);
+	}
+
+	@Override
+	public PlayerAction trainerCanBePlayedFromHand() {
+		return PlayerAction.PLAY_TRAINER_CARD;
+	}
+
+	@Override
+	public void playFromHand() {
+		// Discard trainer card:
+		gameModel.getAttackAction().discardCardToDiscardPile(this.card.getCurrentPosition().getPositionID(), this.card.getGameID(), true);
+	}
+}
