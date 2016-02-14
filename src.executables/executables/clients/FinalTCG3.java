aaef8258148e55a23d596ede3620862f98520694
Michael <mfeldma2@mail.upb.de>
2016-02-14 12:53:08 +0100

    + v 5.0

--- src.executables/executables/clients/FinalTCG3.java
+++ src.executables/executables/clients/FinalTCG3.java
@@ -12,7 +12,7 @@ import model.database.Database;
 import network.tcp.messages.MessageRegister;
 
 public class FinalTCG3 {
-	public static final String VERSION = "4.50";
+	public static final String VERSION = "5.00";
 
 	public static void main(String[] args) {
 		Database.init();
